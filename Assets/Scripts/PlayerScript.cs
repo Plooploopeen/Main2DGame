24364c208f885274ddaac9f1ad9957328612cc2d
Plooploopeen <Kb4S_T.D@proton.me>
2025-10-07 11:24:48 -0700

    walking and jumping and prototype isGrounded

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,4 +1,6 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
@@ -8,11 +10,18 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] PlayerInput playerInput;
     [SerializeField] Rigidbody2D rb;
     [SerializeField] float jumpForce;
+    [SerializeField] float walkForce;
+    [SerializeField] float rayCastLength = 1.5f;
 
     private InputAction jumpAction;
     private InputAction attackAction;
     private InputAction sprintAction;
     private InputAction moveAction;
+    private bool isGrounded;
+
+    private Vector2 velocity;
+    private Vector2 moveDirection;
+
 
     private void Awake()
     {
@@ -24,7 +33,8 @@ public class PlayerScript : MonoBehaviour
 
     void Start()
     {
-        
+        InputActions.FindActionMap("UI").Disable();
+        InputActions.FindActionMap("Gameplay").Enable();
     }
 
     private void OnEnable()
@@ -39,15 +49,45 @@ public class PlayerScript : MonoBehaviour
 
     void Update()
     {
-        if (jumpAction.WasPressedThisFrame())
+
+        moveDirection = moveAction.ReadValue<Vector2>();
+
+        //float leftRayPosition = 
+
+        RaycastHit2D middleHit = Physics2D.Raycast(transform.position, Vector2.down, rayCastLength, LayerMask.GetMask("Ground"));
+        Debug.DrawRay(transform.position, Vector2.down * rayCastLength, Color.red, 1000f);     
+
+        isGrounded = middleHit.collider != null;
+
+        if (isGrounded) 
+        {
+            Debug.Log(isGrounded);
+        }
+
+        if (jumpAction.WasPressedThisFrame() && isGrounded)
         {
             jump();
         }
     }
 
+    void FixedUpdate()
+    {
+        velocity = rb.linearVelocity;
+
+        movePlayer();
+
+        rb.linearVelocity = velocity;
+    }
+
     void jump()
     {
         rb.linearVelocity = Vector2.up * jumpForce;
+    }
+
+    void movePlayer()
+    {
+        float horizontal = moveDirection.x;
+        velocity.x = horizontal * walkForce;
     }
 
 }
