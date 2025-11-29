464f43e32b2bfe7ad9be4410b46258cb4103ef3c
Plooploopeen <Kb4S_T.D@proton.me>
2025-11-29 10:03:17 -0700

    angle matches line and flies in that direction

--- Assets/Scripts/PlayerSwordThrowingScript.cs
+++ Assets/Scripts/PlayerSwordThrowingScript.cs
@@ -1,22 +1,32 @@
 using System.Runtime.InteropServices;
 using Unity.VisualScripting;
 using UnityEditor.Build;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.iOS.Xcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerSwordThrowingScript : MonoBehaviour
 {
-    [SerializeField] GameObject swordPrefafab;
+    [SerializeField] GameObject swordPrefab;
+    [SerializeField] Transform swordTransform;
+
 
     private InputAction aimAction;
     private InputAction throwAction;
+    private GameObject sword;
+    [SerializeField] Rigidbody2D swordRb;
 
     private LineRenderer lineRenderer;
 
     private bool isAiming;
+    private bool canThrow = true;
+    private bool isSwordFlying;
+    [SerializeField] float speed;
     private Vector2 startPoint;
     private Vector2 endPoint;
     private Vector2 aimDirection;
+    private Vector2 velocity;
     [SerializeField] float lineLength;
 
 
@@ -33,9 +43,19 @@ public class PlayerSwordThrowingScript : MonoBehaviour
 
     void Update()
     {
+
         aim();
 
-        checkThrow();
+        if (aimAction.IsPressed() && throwAction.IsPressed() && canThrow)
+        {
+            spawnSword();
+        }
+
+        if (isSwordFlying)
+        {
+            moveSword();
+        }
+
     }
 
     void aim()
@@ -67,11 +87,34 @@ public class PlayerSwordThrowingScript : MonoBehaviour
         }
     }
 
-    void checkThrow()
+    void spawnSword()
     {
-        if (aimAction.IsPressed() && throwAction.IsPressed())
-        {
             Debug.Log("Throw");
-        }
+            // spawn the sword
+            sword = Instantiate(swordPrefab, startPoint, Quaternion.identity);
+
+            // compute direction
+            Vector2 dir = (endPoint - startPoint).normalized;
+
+            // convert direction to angle
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+            // rotate the sword to face that direction
+            sword.transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+
+            //save direction for movemet/force later
+            //var swordScript = sword.GetComponent<Sword>;
+            //swordScript.travelDir = dir;
+
+            canThrow = false;
+            isSwordFlying = true;
+            swordRb = sword.GetComponent<Rigidbody2D>();
+            swordTransform = sword.transform;
+            velocity = aimDirection.normalized * speed;
+    }
+
+    void moveSword()
+    {
+        swordTransform.position += (Vector3)velocity * Time.deltaTime;
     }
 }
