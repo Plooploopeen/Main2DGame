d3f64b09bc40c6637637f083668eb5872db8aaef
Plooploopeen <Kb4S_T.D@proton.me>
2025-12-28 18:47:42 -0700

    eneny can ow find player

--- Assets/Scripts/FirstEnemyAI.cs
+++ Assets/Scripts/FirstEnemyAI.cs
@@ -1,3 +1,4 @@
+//using System.Numerics;
 using UnityEngine;
 
 public class FirstEnemyAI : MonoBehaviour
@@ -6,6 +7,17 @@ public class FirstEnemyAI : MonoBehaviour
 
     [SerializeField] LayerMask layerMask;
     private Rigidbody2D rb;
+    private Animator animator;
+
+    [SerializeField] float detectionRange;
+    [SerializeField] float detectionAngle;
+
+    [SerializeField] GameObject weaponGameObject;
+    [SerializeField] BoxCollider2D weaponCollider;
+
+    private HitBox hitBoxScript;
+
+    private SpriteRenderer spriteRenderer;
 
     [SerializeField] private float speed;
     private Transform playerTransform;
@@ -13,6 +25,10 @@ public class FirstEnemyAI : MonoBehaviour
     private bool isGrounded;
     [SerializeField] float jumpForce;
     [SerializeField] float moveDistance;
+    [SerializeField] float frontRayLength;
+    [SerializeField] LayerMask frontRayLayers;
+    private bool hasSeenPlayer;
+    private bool isFacingRight;
 
     [SerializeField] float rayCastLength;
     [SerializeField] float rayShiftLeftAmount;
@@ -22,14 +38,59 @@ public class FirstEnemyAI : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         enemyHealthScript = GetComponent<NewMonoBehaviourScript>();
+        animator = GetComponent<Animator>();
+        hitBoxScript = weaponGameObject.GetComponent<HitBox>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
     void Start()
     {
         GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
         playerTransform = playerObject.transform;
+        weaponGameObject.SetActive(false);
+        weaponCollider.enabled = false;
+        hitBoxScript = GetComponentInChildren<HitBox>();
     }
 
     void Update()
+    {
+        checkIsGrounded();
+        
+        if (hasSeenPlayer)
+        {
+            chasePlayer();
+            facePlayer();
+            checkAttack();
+        }
+        else
+        {
+            patrol();
+            checkForPlayer();
+        }
+
+
+        // Debug visualization
+        Vector2 enemyForward = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+        float drawDistance = detectionRange;
+
+        // Draw forward direction
+        Debug.DrawRay(transform.position, enemyForward * drawDistance, Color.blue);
+
+        // Draw cone boundaries
+        Vector2 leftBoundary = Quaternion.Euler(0, 0, detectionAngle) * enemyForward;
+        Vector2 rightBoundary = Quaternion.Euler(0, 0, -detectionAngle) * enemyForward;
+
+        Color coneColor = hasSeenPlayer ? Color.red : Color.green;
+        Debug.DrawRay(transform.position, leftBoundary * drawDistance, coneColor);
+        Debug.DrawRay(transform.position, rightBoundary * drawDistance, coneColor);
+
+        // Draw line to player
+        if (playerTransform != null)
+        {
+            Debug.DrawLine(transform.position, playerTransform.position, Color.yellow);
+        }
+    }
+
+    void checkIsGrounded()
     {
         // check isGrounded
         Vector2 leftRayPosition = (Vector2)transform.position + Vector2.left * rayShiftLeftAmount;
@@ -48,7 +109,10 @@ public class FirstEnemyAI : MonoBehaviour
         isGrounded = (middleHit.collider != null && middleHit.collider.CompareTag("Jumpable")) ||
                      (leftHit.collider != null && leftHit.collider.CompareTag("Jumpable")) ||
                      (rightHit.collider != null && rightHit.collider.CompareTag("Jumpable"));
+    }
 
+    void chasePlayer()
+    {
         // check direction and dont move if knocked back
         float direction = Mathf.Sign(playerTransform.position.x - transform.position.x);
         float distance = Mathf.Abs(Vector2.Distance(playerTransform.position, transform.position));
@@ -66,5 +130,81 @@ public class FirstEnemyAI : MonoBehaviour
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
+    }
+
+    void patrol()
+    {
+
+    }
+
+    void checkForPlayer()
+    {
+        float distance = Vector2.Distance(transform.position, playerTransform.position);
+
+        if (distance > detectionRange)
+        {
+            return;
+        }
+
+        Vector2 directionToPlayer = (playerTransform.position - transform.position).normalized;
+        Vector2 enemyForward = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+
+        float angle = Vector2.Angle(enemyForward, directionToPlayer);
+
+        if (angle > detectionAngle)
+        {
+            return;
+        }
+
+        int layermask = ~LayerMask.GetMask("Hurtbox");
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, directionToPlayer, detectionRange, layermask);
+
+        if (hit.collider != null && hit.collider.CompareTag("Player"))
+        {
+            hasSeenPlayer = true;
+        }
+    }
+
+    void facePlayer()
+    {
+        float direction = Mathf.Sign(playerTransform.position.x - transform.position.x);
+        float absScale = Mathf.Abs(transform.localScale.x);
+
+        if (direction > 0)
+        {
+            isFacingRight = true;
+            transform.localScale = new Vector3(absScale, absScale, absScale);
+        }
+        else if (direction < 0)
+        {
+            isFacingRight = false;
+            transform.localScale = new Vector3(-absScale, absScale, absScale);
+        }
+    }
+
+    void checkAttack()
+    {
+        float direction = Mathf.Sign(playerTransform.position.x - transform.position.x);
+
+        RaycastHit2D frontRay = Physics2D.Raycast(transform.position, Vector2.right * direction, frontRayLength, frontRayLayers);
+
+        Debug.DrawRay(transform.position, Vector2.right * direction * frontRayLength, Color.red);
+
+        if (frontRay.collider != null && !animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            animator.Play("Attack");
+        }
+    }
+
+    public void enableHitbox()
+    {
+        weaponGameObject.SetActive(true);
+        weaponCollider.enabled = true;
+    }
+
+    public void disableHitbox()
+    {
+        weaponGameObject.SetActive(false);
+        weaponCollider.enabled = false;
     }
 }
--- Assets/Scripts/TestEnemy.cs
+++ Assets/Scripts/TestEnemy.cs
@@ -6,90 +6,26 @@ public class TestEnemy : MonoBehaviour
 {
 
     [Header("References")]
-    [SerializeField] GameObject weaponGameObject;
-    [SerializeField] BoxCollider2D weaponCollider;
-    private Transform playerTransform;
 
-    private HitBox hitBoxScript;
-
-    private Animator animator;
-    private SpriteRenderer spriteRenderer;
-
-    //-------other---------//
-    [SerializeField] float frontRayLength;
-    [SerializeField] LayerMask frontRayLayers;
 
     private bool isFacingRight;
 
     private void Awake()
     {
-        animator = GetComponent<Animator>();
-        hitBoxScript = weaponGameObject.GetComponent<HitBox>();
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
     }
     void Start()
     {
-        weaponGameObject.SetActive(false);
-        weaponCollider.enabled = false;
-        hitBoxScript = GetComponentInChildren<HitBox>();
 
-        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
-        playerTransform  = playerObject.transform;
     }
 
     void Update()
     {
-        checkForPlayer();
-        facePlayer();
+
     }
 
     private void FixedUpdate()
     {
         
-    }
-
-    void checkForPlayer()
-    {
-        float direction = Mathf.Sign(playerTransform.position.x - transform.position.x);
-
-        RaycastHit2D frontRay = Physics2D.Raycast(transform.position, Vector2.right * direction, frontRayLength, frontRayLayers);
-
-        Debug.DrawRay(transform.position, Vector2.right * direction * frontRayLength, Color.red);
-
-        if (frontRay.collider != null && !animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
-        {
-            animator.Play("Attack");
-        }
-
-
-    }
-
-    void facePlayer()
-    {
-        float direction = Mathf.Sign(playerTransform.position.x - transform.position.x);
-        float absScale = Mathf.Abs(transform.localScale.x);
-
-        if (direction > 0)
-        {
-            isFacingRight = true;
-            transform.localScale = new Vector3(absScale, absScale, absScale);
-        }
-        else if (direction < 0)
-        {
-            isFacingRight = false;
-            transform.localScale = new Vector3(-absScale, absScale, absScale);
-        }
-    }
-
-    public void enableHitbox()
-    {
-        weaponGameObject.SetActive(true);
-        weaponCollider.enabled = true;
-    }
-
-    public void disableHitbox()
-    {
-        weaponGameObject.SetActive(false);
-        weaponCollider.enabled = false;
     }
 }
