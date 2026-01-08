70d1f2d227cadf02904c40dc60b203aeb6ac0128
Plooploopeen <Kb4S_T.D@proton.me>
2026-01-08 11:41:03 -0700

    added multiple choice with controller

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -137,8 +137,6 @@ public class PlayerScript : MonoBehaviour
 
         // Animations
         updateAnimations();
-
-        Debug.Log(isGrounded);
     }
 
     void FixedUpdate()
--- Assets/Scripts/ResponseHandler.cs
+++ Assets/Scripts/ResponseHandler.cs
@@ -1,25 +1,49 @@
-using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
+using System.Buffers;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class ResponseHandler : MonoBehaviour
 {
+    private InputAction DPadAction;
+    private InputAction acceptAction;
+
     [SerializeField] RectTransform responseBox;
     [SerializeField] RectTransform responseButtonTemplate;
     [SerializeField] RectTransform responseContainer;
+    [SerializeField] GameObject selector;
 
     private DialogueUI dialogueUI;
 
     List<GameObject> tempResponseButtons = new List<GameObject>();
 
+    private int selectedIndex = 0;
+    private bool isSelectingResponse;
+    private Response[] selectedResponses;
+
+    private void Awake()
+    {
+        DPadAction = InputSystem.actions.FindAction("DPad");
+        acceptAction = InputSystem.actions.FindAction("Focus");
+    }
+
     private void Start()
     {
         dialogueUI = GetComponent<DialogueUI>();
+        selectedIndex = 0;
+    }
+
+    private void Update()
+    {
+        checkSelectorInput();
     }
 
     public void ShowResponses(Response[] responses)
     {
+        selectedResponses = responses;
+
         float responseBoxHeight = 0;
 
         foreach (Response response in responses)
@@ -37,10 +61,22 @@ public class ResponseHandler : MonoBehaviour
 
         responseBox.sizeDelta = new Vector2(responseBox.sizeDelta.x, responseBoxHeight);    
         responseBox.gameObject.SetActive(true);
+
+        selectedIndex = 0;
+        isSelectingResponse = true;
+
+        if (selector  != null)
+        {
+            selector.SetActive(true);
+            updateSelectorPosition();
+        }
+
     }
 
     private void OnPickedResponse(Response response)
     {
+        isSelectingResponse = false;
+
         responseBox.gameObject.SetActive(false);
 
         foreach (GameObject button in tempResponseButtons)
@@ -51,5 +87,71 @@ public class ResponseHandler : MonoBehaviour
         tempResponseButtons.Clear();
 
         dialogueUI.ShowDialogue(response.DialogueObject);
+    }
+
+    void updateSelectorPosition()
+    {
+        if (selector != null && tempResponseButtons.Count > 0)
+        {
+            selector.transform.position = tempResponseButtons[selectedIndex].transform.position;
+        }
+    }
+
+    void checkSelectorInput()
+    {
+        if (selector != null)
+        {
+            Vector2 DPadDirection = DPadAction.ReadValue<Vector2>();
+
+            if (acceptAction.WasPressedThisFrame())
+            {
+                confirmSelection();
+            }
+
+            if (DPadAction.WasPressedThisFrame())
+            {
+                if (DPadDirection.y < 0)
+                {
+                    selectedIndex++;
+
+                    if (selectedIndex >= tempResponseButtons.Count)
+                    {
+                        selectedIndex = 0;
+                    }
+
+                    updateSelectorPosition();
+                }
+                else if (DPadDirection.y > 0)
+                {
+                    selectedIndex--;
+
+                    if (selectedIndex < 0)
+                    {
+                        selectedIndex = tempResponseButtons.Count - 1;
+                    }
+
+                    updateSelectorPosition();
+                }
+
+                Debug.Log(selectedIndex);
+            }
+        }
+    }
+    
+    void confirmSelection()
+    {
+        if (tempResponseButtons.Count == 0)
+        {
+            return;
+        }
+
+        Response selectedResponse = getResponseAtIndex(selectedIndex);
+
+        OnPickedResponse(selectedResponse);
+    }
+
+    Response getResponseAtIndex(int index)
+    {
+        return selectedResponses[index];
     }
 }
