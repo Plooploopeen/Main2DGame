426f31b091a94f17e18dd5989a75f1426ac39cc2
Plooploopeen <Kb4S_T.D@proton.me>
2026-01-08 21:04:24 -0700

    pauses on puntuation

--- Assets/Scripts/DialogueScripts/TypewriterEffect.cs
+++ Assets/Scripts/DialogueScripts/TypewriterEffect.cs
@@ -3,10 +3,18 @@ using UnityEditor;
 using UnityEngine;
 using TMPro;
 using System;
+using System.Collections.Generic;
+using NUnit.Framework.Api;
 
 public class TypewriterEffect : MonoBehaviour
 {
     [SerializeField] float writingSpeed;
+
+    private readonly Dictionary<HashSet<char>, float> punctuations = new Dictionary<HashSet<char>, float>()
+    {
+        {new HashSet<char>(){'.', '!', '?', }, 0.6f },
+        {new HashSet<char>(){',', ';', ':', }, 0.3f },
+    };
     public Coroutine Run(string textToType, TMP_Text text)
     {
         return StartCoroutine(TypeText(textToType, text));
@@ -21,15 +29,43 @@ public class TypewriterEffect : MonoBehaviour
 
         while (charIndex < textToType.Length)
         {
+            int lastCharacterIndex = charIndex;
+
             t += Time.deltaTime * writingSpeed;
+
             charIndex = Mathf.FloorToInt(t);
             charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
 
-            text.text =  textToType.Substring(0, charIndex);
+            for (int i = lastCharacterIndex; i < charIndex; i++)
+            {
+                bool isLast = i >= textToType.Length - 1;
+
+                text.text = textToType.Substring(0, i + 1);
+
+                if (IsPunctuation(textToType[i], out float waitTime) && !isLast && !IsPunctuation(textToType[i + 1], out _))
+                {
+                    yield return new WaitForSeconds(waitTime);
+                }
+            }
 
             yield return null;
         }
 
         text.text = textToType;
+    }
+
+    private bool IsPunctuation(char character, out float waitTime)
+    {
+        foreach(KeyValuePair<HashSet<char>, float> punctuationCategory in punctuations)
+        {
+            if (punctuationCategory.Key.Contains(character))
+            {
+                waitTime = punctuationCategory.Value;
+                return true;
+            }
+        }
+
+        waitTime = default;
+        return false;
     }
 }
