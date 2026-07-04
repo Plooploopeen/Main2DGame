3c2a54f45064d64a3d388f4f110cde00c80e1d94
Plooploopeen <Kb4S_T.D@proton.me>
2026-07-03 17:17:22 -0700

    working so far...

--- Assets/Scripts/SpellCastingScripts/SpellDataScript.cs
+++ Assets/Scripts/SpellCastingScripts/SpellDataScript.cs
@@ -1,11 +1,11 @@
-using UnityEngine;
+//using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewSpell", menuName = "Spells/Spell Data")]
-public class SpellDataScript : ScriptableObject
-{
-    public string spellName;
-    public Sprite spellIcon;
-    public float spellDamage;
-    public float spellMpCost;
-    public GameObject spellPrefab;
-}
+//[CreateAssetMenu(fileName = "NewSpell", menuName = "Spells/Spell Data")]
+//public class SpellDataScript : ScriptableObject
+//{
+//    public string spellName;
+//    public Sprite spellIcon;
+//    public float spellDamage;
+//    public float spellMpCost;
+//    public GameObject spellPrefab;
+//}
